f99ec2b6cc62a374ef46017c7978a96563465c9c
Mary Miller <marymiller1954@gmail.com>
2016-10-23 17:46:37 -0400

    changes to connection.js for Heroku JawsDB

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Eat-Da-Burger
-Week 14 - UCF Coding Bootcamp - Homework
+A node restaurnat app that allows users to input the  names of burgers they'd like to add.   It also allows the user to add new hamburger choices to the menu.
+The app uses an SQL data base to store the hamburgers on the menu.  It also uses the following NPM packages:
+ Express
+ Express-Handlebars
+ Method-Override
+ Body-Parser
+
--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+}
+connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'moxnix123',
     database: 'burgers_db'
 });
-
+};
 
 connection.connect(function(err) {
     if (err) {
@@ -17,3 +25,4 @@ connection.connect(function(err) {
 });
 
 module.exports = connection;
+
