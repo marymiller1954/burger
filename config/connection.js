f5cc489d3996d77b1306729c66fa5d52b6067449
Mary Miller <marymiller1954@gmail.com>
2016-10-23 17:50:48 -0400

    Add files via upload

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,6 @@ var connection;
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-
-}
 connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
