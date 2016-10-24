f75f2a729cb59d292f2ae9de2af59d1dd7ba5b48
Mary Miller <marymiller1954@gmail.com>
2016-10-24 18:11:29 -0400

    changes for JAWSDB

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,4 +4,16 @@ INSERT INTO burgers (burger_name, devoured, date) VALUES ('Good Burger', false);
 
 INSERT INTO burgers (burger_name, devoured, date) VALUES ('The Big Kahuna Burger', false);
 
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Krusty Burger', false);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Santa Fe', false);
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('The Tel Aviv', false); 
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('The Boulder', false);
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Swiss Mushroom', false);
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('The NC BBQ', false); 
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('The New York Double', false);
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Bacon Cheddar, false);
